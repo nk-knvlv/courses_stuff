02080f6a9205694503f3b62f7056b1d478ccdae1
nk-knvlv <nk_knvlv@mail.ru>
2024-09-26 19:53:29 +0300

    add; algorithms/*; добавил директорию для алгоритмов

---
+++ algorithms/basic_algorithms.py
@@ -0,0 +1,48 @@
+def linear_search(numbers_list: list[int], target_num: int) -> int:
+    for index, num in enumerate(numbers_list):
+        if num is target_num:
+            return index
+    return -1
+
+
+def binary_search(numbers_list, target_num):
+    if not numbers_list:
+        return -1
+    if len(numbers_list) == 1:
+        if numbers_list[0] == target_num:
+            return 0
+        else:
+            return -1
+    low = 0
+    high = len(numbers_list) - 1
+    while low <= high:
+        mid = (low + high) // 2
+
+        if numbers_list[mid] == target_num:
+            return mid
+        if numbers_list[mid] > target_num:
+            high = mid - 1
+        else:
+            low = mid + 1
+    return -1
+
+
+def bubble_sort(num_list):
+    is_swap = True
+    while is_swap:
+        is_swap = False
+        for i, el in enumerate(num_list[:-1]):
+            if num_list[i] > num_list[i + 1]:
+                smaller_value = num_list[i + 1]
+                num_list[i + 1] = num_list[i]
+                num_list[i] = smaller_value
+                is_swap = True
+    return num_list
+
+
+def dumb_sort(num_list: list) -> list:
+    for i, el in enumerate(num_list[:-1]):
+        if num_list[i] > num_list[i + 1]:
+            git
+
+    return num_list
---
+++ algorithms/test_basic_algorithms.py
@@ -0,0 +1,24 @@
+import pytest
+from basic_algorithms import binary_search, bubble_sort
+
+
+@pytest.mark.parametrize("arr, target, expected", [
+    ([], 3, -1),  # Пустой массив
+    ([5], 3, -1),  # Один элемент (отсутствие)
+    ([5], 5, 0),  # Один элемент (присутствие)
+    ([1, 3, 5, 7, 9, 11], 9, 4),  # Массив (присутствие)
+    ([1, 3, 5, 7, 9, 11], 4, -1),  # Массив (отсутствие)
+])
+def test_binary_search(arr, target, expected):
+    assert binary_search(arr, target) == expected
+
+
+@pytest.mark.parametrize("input_array, expected_output", [
+    ([], []),  # Пустой массив
+    ([1], [1]),  # Массив с одним элементом
+    ([5, 4, 3, 2, 1], [1, 2, 3, 4, 5]),  # Массив в обратном порядке
+    ([1, 2, 3, 4, 5], [1, 2, 3, 4, 5]),  # Отсортированный массив
+    ([3, 1, 2, 3, 2, 1], [1, 1, 2, 2, 3, 3])  # Массив с дубликатами
+])
+def test_bubble_sort(input_array, expected_output):
+    assert bubble_sort(input_array) == expected_output
