f99996db042fd72015f3a7d9763645fcc05bbee8
nk-knvlv <nk_knvlv@mail.ru>
2024-12-16 18:11:04 +0300

    update; many; какие то изменения не помню

--- algorithms/basic_algorithms.py
+++ algorithms/basic_algorithms.py
@@ -27,46 +27,116 @@ def binary_search(numbers_list, target_num):
     return -1
 
 
-def bubble_sort(num_list):
+def bubble_sort(num_list: list[int]) -> list[int]:
     is_swap = True
     while is_swap:
         is_swap = False
-        for i, el in enumerate(num_list[:-1]):
+        for i in range(len(num_list) - 1):
             if num_list[i] > num_list[i + 1]:
-                smaller_value = num_list[i + 1]
-                num_list[i + 1] = num_list[i]
-                num_list[i] = smaller_value
+                num_list[i], num_list[i + 1] = num_list[i + 1], num_list[i]
                 is_swap = True
     return num_list
 
 
-def dumb_sort(num_list: list) -> list:
+def dumb_sort(num_list: list[int]) -> list[int]:
     is_swap = True
     while is_swap:
         is_swap = False
-        for i, el in enumerate(num_list[:-1]):
+        for i in range(len(num_list) - 1):
             if num_list[i] > num_list[i + 1]:
-                smaller_value = num_list[i]
-                num_list[i] = num_list[i + 1]
-                num_list[i + 1] = smaller_value
+                num_list[i], num_list[i + 1] = num_list[i + 1], num_list[i]
                 is_swap = True
             continue
     return num_list
 
 
-def shake_sort(num_list: list) -> list:
-    is_swap = True
-    while True:
-        for i, el in enumerate(num_list[:-1]):
-            if num_list[i] > num_list[i + 1]:
-                smaller_value = num_list[i]
-                num_list[i] = num_list[i + 1]
-                num_list[i + 1] = smaller_value
-                break
-        for i, el in enumerate(num_list[:-1:-1]):
-            if num_list[i] > num_list[i + 1]:
-                smaller_value = num_list[i]
-                num_list[i] = num_list[i + 1]
-                num_list[i + 1] = smaller_value
-                is_swap = True
+def shake_sort(num_list: list[int]) -> list[int]:
+    if len(num_list) < 2:
         return num_list
+    low = 0
+    high = len(num_list) - 1
+    while low < high:
+        for i in range(low, high):
+            if num_list[i] > num_list[i + 1]:
+                num_list[i], num_list[i + 1] = num_list[i + 1], num_list[i]
+        high -= 1
+        for i in range(high, low, -1):
+            if num_list[i] < num_list[i - 1]:
+                num_list[i], num_list[i - 1] = num_list[i - 1], num_list[i]
+        low += 1
+    return num_list
+
+
+def even_odd_sort(num_list: list[int]) -> list[int]:
+    if len(num_list) < 2:
+        return num_list
+    even_odd_switch = 0
+    low = 0
+    high = len(num_list) - 1
+    pure_sort_passing = []
+    while len(pure_sort_passing) < 2:
+        pure_sort_passing.append(even_odd_switch)
+        for i in range(low + even_odd_switch, high, 2):
+            if num_list[i] > num_list[i + 1]:
+                num_list[i], num_list[i + 1] = num_list[i + 1], num_list[i]
+                if even_odd_switch in pure_sort_passing:
+                    pure_sort_passing.remove(even_odd_switch)
+        even_odd_switch = 1 - even_odd_switch  # swap even to odd and vice versa.
+    return num_list
+
+
+def comb_sort(num_list: list[int]) -> list[int]:
+    if len(num_list) < 2:
+        return num_list
+    # Общепризнанный оптимальный фактор уменьшения
+    reduction_factor = 1.247
+    count = len(num_list)
+    between_el_dist = count
+    is_swap = True
+    # если проходы с расстоянием больше 1 закончены, нужна финальная проверка классическим пузырьком
+    # которая вызывается условием or between_el_dist > 1
+    while is_swap or between_el_dist > 1:
+        between_el_dist = max(1, int(between_el_dist // reduction_factor))
+
+        high = count - between_el_dist
+
+        is_swap = False
+        for i in range(0, high):
+            if num_list[i] > num_list[i + between_el_dist]:
+                num_list[i], num_list[i + between_el_dist] = num_list[i + between_el_dist], num_list[i]
+                is_swap = True
+    return num_list
+
+
+def insertion_sort(num_list: list[int]) -> list[int]:
+    # моржовый оператор := позволяет внутри выражения присвоить переменную для дальнейшего использования
+    if (length := len(num_list)) < 2:
+        return num_list
+
+    for i in range(1, length):
+        for j in range(i, 0, -1):
+            if num_list[j - 1] > num_list[j]:
+                num_list[j], num_list[j - 1] = num_list[j - 1], num_list[j]
+            else:
+                break
+    return num_list
+
+
+def selection_sort(num_list: list[int]) -> list[int]:
+    if (length := len(num_list)) < 2:
+        return num_list
+
+    for i in range(length):
+        current_max_index = 0
+        for j in range(length - i):
+            if num_list[j] > num_list[current_max_index]:
+                current_max_index = j
+        # no swap if index don't changed
+        if current_max_index != length - i - 1:
+            num_list[length - i - 1], num_list[current_max_index] = num_list[current_max_index], num_list[length - i - 1]
+    return num_list
+
+
+a = [5, 4, 3]
+
+print(selection_sort(a))
--- algorithms/test_basic_algorithms.py
+++ algorithms/test_basic_algorithms.py
@@ -1,5 +1,14 @@
 import pytest
-from basic_algorithms import binary_search, bubble_sort, dumb_sort
+from basic_algorithms import (
+    binary_search,
+    bubble_sort,
+    dumb_sort,
+    shake_sort,
+    even_odd_sort,
+    comb_sort,
+    insertion_sort,
+    selection_sort
+)
 
 
 @pytest.mark.parametrize("arr, target, expected", [
@@ -18,6 +27,7 @@ def test_binary_search(arr, target, expected):
     ([1], [1]),  # Массив с одним элементом
     ([5, 4, 3, 2, 1], [1, 2, 3, 4, 5]),  # Массив в обратном порядке
     ([1, 2, 3, 4, 5], [1, 2, 3, 4, 5]),  # Отсортированный массив
+    ([5, 4, 3], [3, 4, 5]),  # Отсортированный массив
     ([3, 1, 2, 3, 2, 1], [1, 1, 2, 2, 3, 3])  # Массив с дубликатами
 ])
 def test_bubble_sort(input_array, expected_output):
@@ -28,8 +38,91 @@ def test_bubble_sort(input_array, expected_output):
     ([], []),  # Пустой массив
     ([1], [1]),  # Массив с одним элементом
     ([5, 4, 3, 2, 1], [1, 2, 3, 4, 5]),  # Массив в обратном порядке
+    ([5, 4, 3], [3, 4, 5]),  # Отсортированный массив
     ([1, 2, 3, 4, 5], [1, 2, 3, 4, 5]),  # Отсортированный массив
     ([3, 1, 2, 3, 2, 1], [1, 1, 2, 2, 3, 3])  # Массив с дубликатами
 ])
 def test_dumb_sort(input_array, expected_output):
     assert dumb_sort(input_array) == expected_output
+
+
+@pytest.mark.parametrize("input_array, expected_output", [
+    ([], []),  # Пустой массив
+    ([1], [1]),  # Массив с одним элементом
+    ([5, 4, 3], [3, 4, 5]),  # Отсортированный массив
+    ([5, 4, 3, 2, 1], [1, 2, 3, 4, 5]),  # Массив в обратном порядке
+    ([2, 1], [1, 2]),  # Массив в обратном порядке
+    ([1, 2, 3, 4, 5], [1, 2, 3, 4, 5]),  # Отсортированный массив
+    ([3, 1, 2, 3, 2, 1], [1, 1, 2, 2, 3, 3])  # Массив с дубликатами
+])
+def test_shake_sort(input_array, expected_output):
+    assert shake_sort(input_array) == expected_output
+
+
+@pytest.mark.parametrize("input_array, expected_output", [
+    ([], []),  # Пустой массив
+    ([1], [1]),  # Массив с одним элементом
+    ([5, 4, 3], [3, 4, 5]),  # Отсортированный массив
+    ([5, 4, 3, 2, 1], [1, 2, 3, 4, 5]),  # Массив в обратном порядке
+    ([2, 1], [1, 2]),  # Массив в обратном порядке
+    ([1, 2, 3, 4, 5], [1, 2, 3, 4, 5]),  # Отсортированный массив
+    ([3, 1, 2, 3, 2, 1], [1, 1, 2, 2, 3, 3])  # Массив с дубликатами
+])
+def test_even_odd_sort(input_array, expected_output):
+    assert even_odd_sort(input_array) == expected_output
+
+
+@pytest.mark.parametrize("input_array, expected_output", [
+    ([], []),  # Пустой массив
+    ([1], [1]),  # Массив с одним элементом
+    ([5, 4, 3], [3, 4, 5]),  # Отсортированный массив
+    ([5, 4, 3, 2], [2, 3, 4, 5]),  # Отсортированный массив
+    ([5, 4, 3, 2, 1], [1, 2, 3, 4, 5]),  # Массив в обратном порядке
+    ([2, 1], [1, 2]),  # Массив в обратном порядке
+    ([1, 2, 3, 4, 5], [1, 2, 3, 4, 5]),  # Отсортированный массив
+    ([3, 1, 2, 3, 2, 1], [1, 1, 2, 2, 3, 3])  # Массив с дубликатами
+])
+def test_comb_sort(input_array, expected_output):
+    assert comb_sort(input_array) == expected_output
+
+
+@pytest.mark.parametrize("input_array, expected_output", [
+    ([], []),  # Пустой массив
+    ([1], [1]),  # Массив с одним элементом
+    ([5, 4, 3], [3, 4, 5]),  # Отсортированный массив
+    ([5, 4, 3, 2], [2, 3, 4, 5]),  # Отсортированный массив
+    ([5, 4, 3, 2, 1], [1, 2, 3, 4, 5]),  # Массив в обратном порядке
+    ([2, 1], [1, 2]),  # Массив в обратном порядке
+    ([1, 2, 3, 4, 5], [1, 2, 3, 4, 5]),  # Отсортированный массив
+    ([3, 1, 2, 3, 2, 1], [1, 1, 2, 2, 3, 3])  # Массив с дубликатами
+])
+def test_insertion_sort(input_array, expected_output):
+    assert insertion_sort(input_array) == expected_output
+
+
+@pytest.mark.parametrize("input_array, expected_output", [
+    ([], []),  # Пустой массив
+    ([1], [1]),  # Массив с одним элементом
+    ([5, 4, 3], [3, 4, 5]),  # Отсортированный массив
+    ([5, 4, 3, 2], [2, 3, 4, 5]),  # Отсортированный массив
+    ([5, 4, 3, 2, 1], [1, 2, 3, 4, 5]),  # Массив в обратном порядке
+    ([2, 1], [1, 2]),  # Массив в обратном порядке
+    ([1, 2, 3, 4, 5], [1, 2, 3, 4, 5]),  # Отсортированный массив
+    ([3, 1, 2, 3, 2, 1], [1, 1, 2, 2, 3, 3])  # Массив с дубликатами
+])
+def test_selection_sort(input_array, expected_output):
+    assert selection_sort(input_array) == expected_output
+
+
+@pytest.mark.parametrize("input_array, expected_output", [
+    ([], []),  # Пустой массив
+    ([1], [1]),  # Массив с одним элементом
+    ([5, 4, 3], [3, 4, 5]),  # Отсортированный массив
+    ([5, 4, 3, 2], [2, 3, 4, 5]),  # Отсортированный массив
+    ([5, 4, 3, 2, 1], [1, 2, 3, 4, 5]),  # Массив в обратном порядке
+    ([2, 1], [1, 2]),  # Массив в обратном порядке
+    ([1, 2, 3, 4, 5], [1, 2, 3, 4, 5]),  # Отсортированный массив
+    ([3, 1, 2, 3, 2, 1], [1, 1, 2, 2, 3, 3])  # Массив с дубликатами
+])
+def test_selection_sort(input_array, expected_output):
+    assert selection_sort(input_array) == expected_output
